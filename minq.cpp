e32ee391ed7e82cbcbab089e3185e67eea0013c8
Xavier Andrade <xavier@tddft.org>
2020-06-10 13:54:51 -0700

    Changed the interface to receive the number of atoms.

--- minq.cpp
+++ minq.cpp
@@ -45,27 +45,30 @@ int main(int argc, char ** argv){
   assert( err == 0 );
 
   if(rank == 0){  
-    if(argc != 3){
-      std::cerr << "Please use '" << argv[0] << " <nstates> <npoints>'" << std::endl; 
+    if(argc != 2){
+      std::cerr << "Please use '" << argv[0] << " <natoms>'" << std::endl; 
       exit(1);
     }
   }
     
-  long nstates = atoi(argv[1]);
-  long npoints = atoi(argv[2]);
+  long const natoms = atoi(argv[1]);
 
   if(rank == 0){  
-    if(nstates < 0) {
-      std::cerr << "The number of states must be positive. The value received was " << nstates << std::endl; 
-      exit(1);
-    }
-    
-    if(npoints < 0) {
-      std::cerr << "The number of points must be positive. The value received was " << nstates << std::endl; 
+    if(natoms < 0) {
+      std::cerr << "The number of atoms must be positive. The value received was " << natoms << std::endl; 
       exit(1);
     }
   }
+
+  long const nstates = 4*natoms;
   
+  double const vol_per_atom = 125.0;
+  double const volume = vol_per_atom*natoms;
+  double const cell_size = cbrt(volume);
+  double const ecut = 40.0;
+  double const spacing = M_PI*sqrt(0.5/ecut);
+  long const npoints = pow(ceil(cell_size/spacing), 3);
+
   int dims[2] = {0, 0};
   
   err = MPI_Dims_create(size, 2, dims);
@@ -80,11 +83,13 @@ int main(int argc, char ** argv){
   if(rank == 0){
 
     std::cout << "minq input:" << std::endl;
-    std::cout << "  total procs =  " <<  size   << std::endl;
-    std::cout << "  states procs = " << dims[0] << std::endl;
-    std::cout << "  points procs = " << dims[1] << std::endl;
-    std::cout << "  nstates  =     " << nstates << std::endl;
-    std::cout << "  npoints  =     " << npoints << std::endl;
+    std::cout << "  natoms            = " << natoms << std::endl;    
+    std::cout << "  nstates           = " << nstates << std::endl;
+    std::cout << "  npoints           = " << npoints << std::endl;
+    std::cout << "  wavefunction size = " << nstates*npoints*sizeof(std::complex<double>)/(1024.0*1024.0*1024.0) << " GB" << std::endl;
+    std::cout << "  total procs       = " <<  size   << std::endl;
+    std::cout << "  states procs      = " << dims[0] << std::endl;
+    std::cout << "  points procs      = " << dims[1] << std::endl;
 
   }
 
