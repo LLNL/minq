068fb45ec4dbda1bfc7bef1590c05e5c1cf9098b
Xavier Andrade <xavier@tddft.org>
2020-06-11 04:08:04 -0700

    Make the orthogonalization check work in parallel.

--- orthogonalize.hpp
+++ orthogonalize.hpp
@@ -26,6 +26,7 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "overlap.hpp"
 
+#include <mpi.h>
 #include <slate/slate.hh>
 
 #include <iostream>
@@ -62,12 +63,20 @@ namespace aux {
 template <class Type>
 auto check_orthogonalization(slate::Matrix<Type>  & wavefunction){
 
+  int rank;
+  auto ierr = MPI_Comm_rank(wavefunction.mpiComm(), &rank);
+  assert(ierr == 0);
+
+  if(rank == 0) std::cout << "Checking orthogonalization:";
+  
   int nprocs[2];
   int periods[2];
   int coords[2];
   
   auto err = MPI_Cart_get(wavefunction.mpiComm(), 2, nprocs, periods, coords);
   assert(err == 0);  
+
+  MPI_Barrier(wavefunction.mpiComm());
   
   auto nstates = wavefunction.m();
   auto nbs = (nstates + nprocs[0] - 1)/nprocs[0];
@@ -75,33 +84,53 @@ auto check_orthogonalization(slate::Matrix<Type>  & wavefunction){
   slate::HermitianMatrix<Type> overlap(slate::Uplo::Lower, nstates, nbs, nprocs[0], nprocs[1], wavefunction.mpiComm());
   overlap.insertLocalTiles();
 
+
+  for(long jj = 0; jj < overlap.nt(); jj++) {
+    for(long ii = 0; ii < overlap.mt(); ii++) {
+      if(not overlap.tileExists(ii, jj)) continue;
+      auto tile = overlap(ii, jj);
+      
+      for(long jtile = 0; jtile < tile.nb(); jtile++){
+        for(long itile = 0; itile < tile.mb(); itile++){
+          tile.data()[itile + tile.stride()*jtile] = NAN;
+        }
+      }
+    }
+  }
+ 
   minq::overlap(wavefunction, overlap);
 
   double diff = 0.0;
   
-  for (long jj = 0; jj < overlap.nt(); ++jj) {
-    for (long ii = 0; ii < overlap.mt(); ++ii) {
-      if (overlap.tileIsLocal( ii, jj )) {
-        auto tile = overlap(ii, jj);
+  for(long jj = 0; jj < overlap.nt(); jj++) {
+    for(long ii = 0; ii < overlap.mt(); ii++) {
+      if(not overlap.tileExists(ii, jj)) continue;
+      auto tile = overlap(ii, jj);
+      
+      for(long jtile = 0; jtile < tile.nb(); jtile++){
+        for(long itile = 0; itile < tile.mb(); itile++){
 
-        for(long jtile = 0; jtile < tile.nb(); jtile++){
-          for(long itile = jtile; itile < tile.mb(); itile++){
-            if(itile == jtile and ii == jj){
-              diff += fabs(tile.data()[itile + tile.stride()*jtile] - 1.0);
-            } else {
-              diff += fabs(tile.data()[itile + tile.stride()*jtile]);
-            }
+          if(ii == jj and itile < jtile) continue;
+          
+          if(itile == jtile and ii == jj){
+            diff += fabs(tile.data()[itile + tile.stride()*jtile] - 1.0);
+          } else {
+            diff += fabs(tile.data()[itile + tile.stride()*jtile]);
           }
+          
         }
-
       }
+
     }
   }
-
+  
+  err = MPI_Allreduce(MPI_IN_PLACE, &diff, 1, MPI_DOUBLE, MPI_SUM, wavefunction.mpiComm());
+  assert(err == 0);
+                   
   if(diff <= 1e-10){
-    std::cout << "  Orthogonalization: [  OK  ] " << std::endl;
+    if(rank == 0) std::cout << " [  OK  ] ( diff = " << diff << ") " << std::endl;
   } else {
-    std::cout << "  Orthogonalization: [ FAIL ] " << std::endl;
+    if(rank == 0) std::cout << " [ FAIL ] ( diff = " << diff << ") " << std::endl;
   }
   
 }
