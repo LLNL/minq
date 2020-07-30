713e5092594a01868f1350427d9b12714bb0c0d9
Xavier Andrade <xavier@tddft.org>
2020-07-29 17:19:45 -0700

    Replaced the license notice.

--- overlap.hpp
+++ overlap.hpp
@@ -1,28 +1,10 @@
 #ifndef MINQ__OVERLAP
 #define MINQ__OVERLAP
 
-/* 
-Copyright 2020 Xavier Andrade <xavier@llnl.gov>
-
-Redistribution and use in source and binary forms, with or without modification, are permitted
-provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright notice, this list of conditions
-   and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
-   and the following disclaimer in the documentation and/or other materials provided with the
-   distribution.
-3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse
-   or promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
-IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+// Copyright 2020 Lawrence Livermore National Security, LLC and other
+// minq developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: BSD-3-Clause
 
 #include <slate/slate.hh>
 
